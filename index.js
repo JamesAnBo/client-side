f18ab29dd8a9fe24f9cc4be2ff3cb00da11d1aa7
James_B <aesk.99@gmail.com>
2016-08-09 11:53:05 +1200

    Updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-var issData = data.name
+var issData = JSON.parse(data.body).name
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'James', issData: issData})
