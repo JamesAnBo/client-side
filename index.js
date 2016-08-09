382fd9be39906774b05901d030f5f637037a112e
James_B <aesk.99@gmail.com>
2016-08-09 16:09:53 +1200

    Updates

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ var greeting = require('./views/greeting.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-var issData = JSON.parse(data.body)
-  // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'James', issData: issData})
-})
 
 function refresh (){
-  window.location.reload()
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
+
+    // In case you're curious
+    console.log(data.body) // FYI: data.body is a string
+  var issData = JSON.parse(data.body)
+    // Replace 'Space' below with the response
+    var target = document.getElementsByTagName('main')[0]
+    target.innerHTML = greeting({name: 'James', issData: issData})
+  })
 }
 
 document.getElementById('refresh_button').addEventListener('click', refresh)
